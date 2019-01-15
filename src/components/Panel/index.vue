2d4e930abd1db932594a108f75bc1211e9b66373
hq <843804874@qq.com>
2019-01-15 17:25:37 +0800

    基于vuecli-3的组件库开发

---
+++ src/components/Panel/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="panel">
+      {{txt}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cmsPanel',
+  data () {
+    return {
+      txt: '面板组件'
+    }
+  }
+}
+</script>
+
+ <style lang="scss" scoped>
+  .panel{
+   width: 100px;
+   height: 200px;
+   background: red;
+  }
+</style>
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  runtimeCompiler: true,
+  baseUrl: '',
+  outputDir: undefined,
+  assetsDir: './static',
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+
+}
